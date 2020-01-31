30cfd5d4b2847d26cd0bfd437a1e432f69389e36
Phillip Clark <pcclark4@gmail.com>
2020-01-30 16:53:17 -0800

    Updated readme. Removed unused typedef.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cggml
+
+A geometry-ish library written in C.
+
+Goals
+- Small and simple
+- C90 compliant
+- MISRA-C friendly
+- Usable in applications targeting Game Boys, Super Nintendos, toasters, refrigerators, and modern PCs
--- include/sort.h
+++ include/sort.h
@@ -5,7 +5,6 @@
 #include "stddef.h"
 
 typedef int32_t (*comparator_func)(const void *, const void *);
-typedef int8_t (*equality_func)(const void *, const void *);
 
 void sort_insertion(
     void *arr, uint32_t arrSize, size_t elementSize, comparator_func cmp);
